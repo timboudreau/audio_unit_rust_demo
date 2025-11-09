6ec621eafceac417626a7f84057222dd23a09d62
Tim Boudreau <tim@timboudreau.com>
2025-11-09 17:36:05 -0500

    In order to use newer swift, the app may no longer be a class

--- macos/RustDspInProcessDemo/RustDspInProcessDemo/RustDspInProcessDemoApp.swift
+++ macos/RustDspInProcessDemo/RustDspInProcessDemo/RustDspInProcessDemoApp.swift
@@ -2,10 +2,8 @@ import CoreMIDI
 import SwiftUI
 
 @main
-class RustDspInProcessDemoApp: App {
+struct RustDspInProcessDemoApp: App {
     @ObservedObject private var hostModel = AudioUnitHostModel()
-
-    required init() {}
 
     var body: some Scene {
         WindowGroup {
